6d5f467750a1229656002f164137593e7a49969c
An Nguyen <tranminhannguyen@gmail.com>
2024-08-10 18:38:08 +0700

    Update utils

---
+++ lib/utils.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+extension TakeIf<T> on T? {
+  /// Return the current value if the given block is satisifed, will return
+  /// `null` if not.
+  T? takeIf(bool Function(T it) block) {
+    return switch (this) {
+      var _this? => block(_this) ? _this : null,
+      _ => null
+    };
+  }
+}
+
+extension Let<T> on T {
+  /// Calls the specified `block` with the current value as its argument.
+  R let<R>(R Function(T it) block) {
+    return block(this);
+  }
+}
