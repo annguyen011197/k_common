50ef083012afb1a56387b42c7e4d7b1b8355c13e
An Nguyen <tranminhannguyen@gmail.com>
2024-09-02 01:44:05 +0700

    Update

--- lib/string_ex.dart
+++ lib/string_ex.dart
@@ -1,3 +1,32 @@
 extension StringOpEx on String? {
   bool get isNullOrEmpty => this == null || this!.isEmpty;
 }
+
+extension StringEx on String {}
+
+extension IntEx on int {
+  Map<String, int> parseDuration() {
+    int totalDays = this;
+    const int daysInYear = 365;
+    const int daysInMonth =
+        30; // Approximate, considering all months have 30 days
+
+    int years = totalDays ~/ daysInYear;
+    int remainingDays = totalDays % daysInYear;
+
+    int months = remainingDays ~/ daysInMonth;
+    remainingDays = remainingDays % daysInMonth;
+
+    Map<String, int> durationMap = {
+      'year': years,
+      'month': months,
+      'day': remainingDays
+    };
+
+    return durationMap;
+  }
+
+  String toFormatString({int minCharCount = 2}) {
+    return toString().padLeft(minCharCount, '0');
+  }
+}
