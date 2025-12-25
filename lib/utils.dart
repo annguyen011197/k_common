9c45d9f99e4164e3be90855a759bc27fe3b439a4
An Nguyen <tranminhannguyen@gmail.com>
2025-12-25 22:07:43 +0700

    Add helper function

--- lib/utils.dart
+++ lib/utils.dart
@@ -4,7 +4,7 @@ extension TakeIf<T> on T? {
   T? takeIf(bool Function(T it) block) {
     return switch (this) {
       var _this? => block(_this) ? _this : null,
-      _ => null
+      _ => null,
     };
   }
 }
@@ -13,5 +13,28 @@ extension Let<T> on T {
   /// Calls the specified `block` with the current value as its argument.
   R let<R>(R Function(T it) block) {
     return block(this);
+  }
+}
+
+extension Intersperse<E> on List<E> {
+  List<E> intersperse(E separator) {
+    if (isEmpty) return [];
+    return List.generate(
+      length * 2 - 1,
+      (i) => i.isEven ? this[i ~/ 2] : separator,
+    );
+  }
+}
+
+extension IntersperseIterable<E> on Iterable<E> {
+  Iterable<E> intersperse(E separator) sync* {
+    final iterator = this.iterator;
+    if (!iterator.moveNext()) return;
+
+    yield iterator.current;
+    while (iterator.moveNext()) {
+      yield separator;
+      yield iterator.current;
+    }
   }
 }
